f5d835c5c200d2e4f5d67edcb956312345364a5c
NAGESH ADLURU <adluru@wisc.edu>
2019-11-10 12:54:29 -0600

    flair,t1,dwicontrasts etc

--- Groundwork_Img.sh
+++ Groundwork_Img.sh
@@ -189,6 +189,13 @@ train=$configdir/train/trainConfig_uw_v2.cfg
 ./deepMedicRun -model $model -train $train -dev cuda1
 # endregion
 
+# region training take 3 closeop
+configdir=/home/adluru/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles
+model=$configdir/models/modelConfig_uw_v2_closeop.cfg
+train=$configdir/train/trainConfig_uw_v2_closeop.cfg
+./deepMedicRun -model $model -train $train -dev cuda1
+# endregion
+
 # region training take 4
 cd /scratch/adluru/sp_adluru/JIMData/GTLabels
 parallel -j1 -k echo $(pwd)/{} ::: *closeop.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/validation/validationGtLabels_v3.cfg
@@ -197,6 +204,15 @@ configdir=/home/adluru/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfig
 model=$configdir/models/modelConfig_uw.cfg
 train=$configdir/train/trainConfig_uw_v3.cfg
 ./deepMedicRun -model $model -train $train -dev cuda0
+# endregion
+
+# region testing n=65 using modelConfig_uw take 4
+outdir=/scratch/adluru/sp_adluru/DMOrigOutputUWV4
+configdir=/home/adluru/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles
+./deepMedicRun -model $configdir/models/modelConfig_uw.cfg \
+    -test $configdir/test/testConfig_take4.cfg \
+    -load $outdir/saved_models/trainSessionDmOriginal/deepMedicOriginal.trainSessionDmOriginal.final.2019-10-12.01.30.45.140821.model.ckpt \
+    -dev cuda0
 # endregion
 
 # region tracking training progress
@@ -281,4 +297,90 @@ parallel -j30 --bar -k --plus 'fslmaths {} -thr $(fslstats {} -P 80) {..}_size_t
 parallel --dry-run -j30 -k --bar --plus cluster -i {} -t 1 -o {..}_index.nii.gz --osize={..}_size.nii.gz ::: *Mask.nii
 parallel --dry-run -j30 --bar -k --plus 'fslmaths {} -thr $(fslstats {} -P 80) -bin {..}_size_thr.nii.gz' ::: *_size.nii.gz
 parallel --dry-run -j30 --bar -k --plus 'fslmaths {} -dilM -ero -bin -dilM -ero -bin {..}_closeop.nii.gz' ::: *_size_thr.nii.gz
+# endregion
+
+# region prepping to train with ALMs (acute lesion masks)
+cd /scratch/adluru/sp_adluru/JIMData/GTLabels_ALM
+parallel --bar 'arr=($(fslstats {} -V));[[ ${arr[0]} -eq 0 ]] && rm {};' ::: *Acute_Lesion_Mask.nii
+parallel -j30 -k --bar --plus cluster -i {} -t 1 -o {..}_index.nii.gz --osize={..}_size.nii.gz ::: *Mask.nii
+parallel -j30 --bar -k --plus 'fslmaths {} -thr $(fslstats {} -P 80) -bin {..}_thr.nii.gz' ::: *_size.nii.gz
+# no close operation for the alm.
+#parallel -j30 --bar -k --plus 'fslmaths {} -dilM -ero -bin -dilM -ero -bin {..}_closeop.nii.gz' ::: *_thr.nii.gz
+# endregion
+
+# region train validation config files for ALM training
+cd /scratch/adluru/sp_adluru/JIMData/GTLabels_ALM
+parallel -j1 -k echo $(pwd)/{} ::: *thr.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainGtLabels_ALM.cfg
+parallel -j1 -k 'fn={/};echo $(pwd)/T1WImages/${fn%_Acute*}_BRAVO_uniform_normalized_BFC_brain_demean_destd.nii.gz' ::: GTLabels_ALM/*thr.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainChannels_t1c_ALM.cfg
+parallel -j1 -k 'fn={/};echo $(pwd)/T1WBrainMasks/${fn%_Acute*}_BRAVO_uniform_normalized_BFC_brain_mask.nii.gz' ::: GTLabels_ALM/*thr.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainRoiMasks_ALM.cfg
+
+
+ls GTLabels_ALM/*size_thr.nii.gz | parallel -k echo $(pwd)/{} > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainGtLabels_ALM_Nov082019.cfg
+ls GTLabels_ALM/*size_thr.nii.gz | sed 's/.*_ALM\///g;s/_Acute.*//g' | parallel -k echo $(pwd)/T1WImages/{}_BRAVO_uniform_normalized_BFC_brain_demean_destd.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainChannels_t1c_ALM_Nov082019.cfg
+ls GTLabels_ALM/*size_thr.nii.gz | sed 's/.*_ALM\///g;s/_Acute.*//g' | parallel -k echo $(pwd)/T1WBrainMasks/{}_BRAVO_uniform_normalized_BFC_brain_mask.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainRoiMasks_ALM_Nov082019.cfg
+
+parallel -k 'echo $(pwd)/dwiinstruc/{1}_nozfi_b1bc_rc_withgrad_{2}_instruc.nii.gz >> ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainChannels_{2}_ALM_Nov082019.cfg' ::: $(ls JIMData/GTLabels_ALM/*size_thr.nii.gz | sed 's/.*_ALM\///g;s/_Acute.*//g') ::: md rd ad fa dwimin dwimean b0mean dwistd dwirms
+
+ls JIMData/GTLabels_ALM/*size_thr.nii.gz | sed 's/.*_ALM\///g;s/_Acute.*//g' | parallel -k echo $(pwd)/T1WBrainMasks/{}.flair_stdorientation_BFC_bet_in_struc.nii.gz > ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainChannels_flair_ALM_Nov092019.cfg
+
+ls train/trainChannels_*Nov*.cfg | parallel -k --bar 'sed -n -e 3p -e 6p -e 9p -e 12p -e 18p -e 24p {} > {//}/validation/{/.}_validation.cfg'
+ls train/trainChannels_*Nov*.cfg | parallel -k --bar 'sed -i.bak -e "3d;6d;9d;12d;18d;24d" {}'
+
+ls train/trainGtLabels_ALM_Nov082019.cfg | parallel -k --bar 'sed -n -e 3p -e 6p -e 9p -e 12p -e 18p -e 24p {} > {//}/validation/{/.}_validation.cfg'
+ls train/trainGtLabels_ALM_Nov082019.cfg | parallel -k --bar 'sed -i.bak -e "3d;6d;9d;12d;18d;24d" {}'
+
+ls train/trainRoiMasks_ALM_Nov082019.cfg | parallel -k --bar 'sed -n -e 3p -e 6p -e 9p -e 12p -e 18p -e 24p {} > {//}/validation/{/.}_validation.cfg'
+ls train/trainRoiMasks_ALM_Nov082019.cfg | parallel -k --bar 'sed -i.bak -e "3d;6d;9d;12d;18d;24d" {}'
+
+ls train/trainGtLabels_ALM_Nov082019.cfg.bak | parallel -k --bar 'sed -n -e 3p -e 6p -e 9p -e 12p -e 18p -e 24p {}|sed "s/.*_ALM\///g;s/\_Ac.*//g"' | parallel echo {}_Pred_ALM.nii.gz > train/validation/validationNamesOfPredictions_ALM_Nov092019.cfg
+# endregion
+
+# region training ALM Nov 9, 2019.
+./deepMedicRun -model ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/models/modelConfig_uw_ALM.cfg -train ~/StrokeAndDiffusionProject/uwstrokeproject/DeepMedicConfigFiles/train/trainConfig_uw_ALM.cfg -dev cuda1
+# endregion
+
+# region n=65 generate the appropriate dwi contrast based on the Stroke 2019 paper that Veena sent
+cd /scratch/adluru/sp_adluru/dti
+parallel -j24 --bar 'dwiextract {} -shells 0 - | mrmath - mean {.}_b0mean.nii.gz -axis 3' ::: *withgrad.mif
+parallel -j24 --bar -k 'dwiextract {} -shells 2000 - | mrmath - mean dwicontrasts/{/.}_dwimean.nii.gz -force -axis 3' ::: dti/*withgrad.mif
+ parallel -j24 --bar -k --dry-run 'dwiextract {1} -shells 2000 - | mrmath - {2} dwicontrasts/{1/.}_dwi{2}.nii.gz -force -axis 3' ::: dti/*withgrad.mif ::: min max std norm rms
+# endregion
+# region n=65 register that dwi contrast and the rest of the DTI measures to t1w
+
+parallel --dry-run --plus -j24 --bar epi_reg -v --epi={1} --t1={2} --t1brain={3} --out=epi2struc/{1/..}_epi2struc --noclean ::: dti/*b0mean.nii.gz :::+ T1/*BFC.nii.gz :::+ T1/*BFC_brain.nii.gz
+cd /scratch/adluru/sp_adluru/epi2struc
+parallel -j20 --bar convert_xfm -omat {.}_inv.mat -inverse {} ::: *epi2struc.mat
+
+parallel -j24 --bar -k --plus --dry-run flirt -in {1} -ref {2} -applyxfm -init epi2struc/{1/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dti/*b0mean.nii.gz :::+ T1/*BFC_brain.nii.gz
+parallel -j24 --bar -k --plus --dry-run flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dti/*withgrad_fa.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus --dry-run flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dti/*withgrad_md.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus --dry-run flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dti/*withgrad_ad.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus --dry-run flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dti/*withgrad_rd.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+
+parallel -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dwicontrasts/*dwimean.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dwicontrasts/*dwimin.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dwicontrasts/*dwimax.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dwicontrasts/*dwirms.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+parallel -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init epi2struc/{3/..}_epi2struc.mat -out dwiinstruc/{1/..}_instruc.nii.gz ::: dwicontrasts/*dwistd.nii.gz :::+ T1/*BFC_brain.nii.gz :::+ dti/*b0mean.nii.gz
+
+cd /scratch/adluru/sp_adluru/dwiinstruc
+parallel --dry-run --plus -j24 -k --bar 'arr=($(mrstats {} -quiet -output mean -output std -ignorezero));mrcalc {} ${arr[0]} -sub ${arr[1]} -div {..}_demean_destd.nii.gz' ::: *.nii.gz
+
+epi_reg -v --epi=dti/KS001V1_nozfi_b1bc_rc_withgrad_b0mean.nii.gz --t1=T1/KS001V1.anat_stdorientation_normalized_BFC.nii.gz --t1brain=T1/KS001V1.anat_stdorientation_normalized_BFC_brain.nii.gz --out=KS001V1epi2struc --noclean
+
+convert_xfm -omat KS001V1epi2struc_inv.mat -inverse KS001V1epi2struc.mat
+flirt -in T1/KS001V1.anat_stdorientation_normalized_BFC_brain.nii.gz -ref dti/KS001V1_nozfi_b1bc_rc_withgrad_b0mean.nii.gz -applyxfm -init KS001V1epi2struc_inv.mat -out KS001V1strucinepi.nii.gz
+# endregion
+
+# region FLAIR pre-processing
+cd /scratch/adluru/sp_adluru/flair
+parallel -j30 --bar fslreorient2std {} {.}_stdorientation.nii.gz ::: *flair.nii
+parallel --dry-run --bar --plus -j24 -k N4BiasFieldCorrection -d 3 -i {} -o [{..}_BFC.nii.gz,{..}_BF.nii.gz] -r -s 2 ::: *flair_stdorientation.nii.gz
+ls KS*028* | parallel 'cp {} $(echo {}|sed "s/flair/flair\_stdorientation/g")'
+parallel --dry-run -j30 --bar --plus bet {} {..}_bet -f 0.6 -m -R ::: *flair_stdorientation_BFC.nii.gz
+parallel --dry-run -j24 --bar -k --plus flirt -in {1} -ref {2} -cost normmi -searchcost normmi -omat flair2struc/{1/..}_to_{2/..}.mat ::: flair/*stdorientation_BFC_bet.nii.gz :::+ T1/*anat*stdorientation*BFC_brain.nii.gz
+parallel --dry-run -j24 --bar -k --plus flirt -in {1} -ref {2} -applyxfm -init flair2struc/{1/..}_to_{2/..}.mat -out flairinstruc/{1/..}_in_struc.nii.gz ::: flair/*BFC_bet.nii.gz :::+ T1/*BFC_brain.nii.gz
+
+
+ls flair/*stdorientation_BFC_bet.nii.gz | sed 's/.*flair\///g;s/.flair.*//g' | grep -v SS064V1 | parallel --dry-run -j24 --rpl '{_} s/\_//g' --bar -k flirt -in flair/{}.flair_stdorientation_BFC_bet.nii.gz -ref T1/{_}.anat_stdorientation_normalized_BFC_brain.nii.gz -cost normmi -searchcost normmi -omat flair2struc/{_}_flair2struc.mat
 # endregion
